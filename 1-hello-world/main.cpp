e67e85fde0e8c62108efea12d10e456bdd4384f8
Melih <digitaloceanmb@gmail.com>
2024-10-06 01:27:19 +0300

    Initial commit

---
+++ 1-hello-world/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+// Bu ifade std kütüpahensini kod içerisinde yazmayı zorunlu tutmadan kod yazmamızı sağlıyor.
+using namespace std;
+
+int main() {
+  // cout, standart output için linklenmiş bir objedir. << operatörü ile bir output vermesini sağlayabiliyoruz.
+  cout << "Hello, World!" << endl;
+
+  return 0;
+}
